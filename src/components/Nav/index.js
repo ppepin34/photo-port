665fe978fe00ba4f5cda6e22171fcc555858c838
ppepin34 <ppepin34@gmail.com>
2022-07-13 19:20:19 -0400

    contact form working

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,6 +7,8 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
   useEffect(() => {
@@ -29,19 +31,21 @@ function Nav(props) {
           <li className="mx-2">
             <a
               href="#about"
+              onClick={() => setContactSelected(false)}
             >
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {
             categories.map((category) => (
-              <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'
+              <li className={`mx-1 ${currentCategory.name === category.name && !contactSelected && 'navActive'
             }`} key={category.name} >
                 <span onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
+                  setContactSelected(false);
                 }}>
                   {capitalizeFirstLetter(category.name)}
                 </span>
